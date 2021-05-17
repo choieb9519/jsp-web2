c0560d6cf44165c6ca62f91025d5268cb80d7102
choieb9519 <choieb9519@gmail.com>
2021-05-17 15:05:49 +0900

    20210517

---
+++ src/ch08/book.java
@@ -0,0 +1,44 @@
+package ch08;
+
+public class book {
+	private String code;
+	private String title;
+	private String writer;
+	private String publisher;
+	private int price;
+	
+	//java beans
+	//get set이 세트로 있어야 하는건 아님, 다 없어도 괜찮음
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	
+}

48b9e1bcd035d630ada230bf0de32395b81e450b
choieb9519 <choieb9519@gmail.com>
2021-05-27 15:37:14 +0900

    20210527

---
+++ WebContent/WEB-INF/sql/sql.sql
@@ -0,0 +1,15 @@
+SELECT CustomerName,City FROM Customers;
+SELECT * FROM Customers;
+SELECT customername FROM Customers;
+/* 관습상 키워드는 대문자로*/
+-- 한 줄 주석(ctrl/)
+
+SELECT City FROM Customers;
+SELECT * FROM Categories;
+SELECT * FROM Customers;
+SELECT * FROM Employees;
+SELECT * FROM OrderDetails;
+SELECT * FROM Orders;
+SELECT * FROM Products;
+SELECT * FROM Shippers;
+SELECT * FROM Suppliers;

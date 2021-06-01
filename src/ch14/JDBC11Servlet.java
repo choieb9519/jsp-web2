3e440d60cd1818eebcb5f15f4c43b61c16356186
choieb9519 <choieb9519@gmail.com>
2021-06-01 19:05:50 +0900

    20210601

--- src/ch14/JDBC11Servlet.java
+++ src/ch14/JDBC11Servlet.java
@@ -13,8 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import ch14.bean.Employee;
-
 /**
  * Servlet implementation class JDBC11Servlet
  */

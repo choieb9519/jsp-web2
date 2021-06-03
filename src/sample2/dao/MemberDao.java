ab977da769de705f48cafe3caf132b005f47f823
choieb9519 <choieb9519@gmail.com>
2021-06-03 10:36:45 +0900

    20210603

--- src/sample2/dao/MemberDao.java
+++ src/sample2/dao/MemberDao.java
@@ -144,5 +144,53 @@ public class MemberDao {
 		
 		return null;
 	}
+
+	public boolean update(Member member) {
+		String sql = "UPDATE Member "
+				+ "SET password = ?, "
+				+ "    name = ?, "
+				+ "    birth = ? "
+				+ "WHERE id = ? ";
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+
+			con = DriverManager.getConnection(url, user, password);
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.setString(1, member.getPassword());
+			pstmt.setString(2, member.getName());
+			pstmt.setDate(3, member.getBirth());
+			pstmt.setString(4, member.getId());
+			
+			int cnt = pstmt.executeUpdate();
+			
+			if (cnt > 0) {  //중복되는 아이디 없게하기
+				return true;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return false;
+	}
 	
 }

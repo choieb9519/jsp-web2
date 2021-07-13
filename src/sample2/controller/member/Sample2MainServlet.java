c78e1a42d932348362ccccd942278912fec1426d
choieb9519 <choieb9519@gmail.com>
2021-07-13 13:26:22 +0900

    20210713

--- src/sample2/controller/member/Sample2MainServlet.java
+++ src/sample2/controller/member/Sample2MainServlet.java
@@ -6,10 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class Sample2MainServlet
- */
+ /** 
+  * Servlet implementation class Sample2MainServlet
+  */
 @WebServlet("/sample2/main")
 public class Sample2MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;

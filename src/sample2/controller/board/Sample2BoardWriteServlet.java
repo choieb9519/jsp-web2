289405573c703b5b3fd414dad7b1fc5e37a5777d
choieb9519 <choieb9519@gmail.com>
2021-07-14 08:59:13 +0900

    merge

--- src/sample2/controller/board/Sample2BoardWriteServlet.java
+++ src/sample2/controller/board/Sample2BoardWriteServlet.java
@@ -62,8 +62,4 @@ public class Sample2BoardWriteServlet extends HttpServlet {
 			request.getRequestDispatcher(path).forward(request, response);
 		}
 	}
-<<<<<<< HEAD
-
-=======
->>>>>>> branch 'master' of https://github.com/choieb9519/jsp-web2.git
 }
